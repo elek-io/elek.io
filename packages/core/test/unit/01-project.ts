24c4b304aa94055910167fc8431ba70bb748a6f9
Nils Kolvenbach <info@nils-kolvenbach.de>
2022-10-04 00:13:12 +0200

    Should fix pipeline tests

---
+++ packages/core/test/unit/01-project.ts
@@ -0,0 +1,10 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+describe('Unit tests', async () => {
+
+  it('none created yet', async () => {
+    expect(true).to.be.true;
+  });
+
+});
