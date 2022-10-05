0d8ee2c3d8ebbdb0558859533860f03586ba3071
Nils Kolvenbach <info@nils-kolvenbach.de>
2022-10-05 20:32:13 +0200

    Trying src config

--- packages/core/test/unit/01-project.ts
+++ packages/core/test/unit/01-project.ts
@@ -2,9 +2,7 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
 describe('Unit tests', async () => {
-
   it('none created yet', async () => {
     expect(true).to.be.true;
   });
-
 });
