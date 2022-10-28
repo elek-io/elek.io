2201cd329d543775d705bc4144b247f94d885341
Nils Kolvenbach <info@nils-kolvenbach.de>
2022-10-28 17:19:48 +0200

    Should fix the CI build

--- apps/client/src/renderer/preload.ts
+++ apps/client/src/renderer/preload.ts
@@ -1,12 +1,14 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-import ProjectService from 'core/dist/esm/service/ProjectService';
 import { contextBridge, ipcRenderer } from 'electron';
 
+/**
+ * @todo Ideally we could import and use the core types here
+ */
 export interface ContextBridgeApi {
   core: {
     projects: {
-      count: ProjectService['count'];
+      count: () => Promise<number>;
     };
   };
 }
