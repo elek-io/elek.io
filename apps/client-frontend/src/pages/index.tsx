c37f5ae1c5102bbc8e1a05461e6f6cf6f8a6fcdb
Nils Kolvenbach <info@nils-kolvenbach.de>
2022-10-31 14:39:46 +0100

    Added @elek.io prefix to all packages

--- apps/client-frontend/src/pages/index.tsx
+++ apps/client-frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Button } from 'ui';
+import { Button } from '@elek.io/ui';
 
 const Home: NextPage = () => {
   const getCount = async () => {
