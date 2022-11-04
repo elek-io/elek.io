23a290f0cff84e823bd8ed97b8576eb212f88b5b
Nils Kolvenbach <info@nils-kolvenbach.de>
2022-11-04 23:51:12 +0100

    Removed internal linters for super linter (test)

--- apps/client/src/main/index.ts
+++ apps/client/src/main/index.ts
@@ -24,7 +24,6 @@ class Main {
 
     // Handle creating/removing shortcuts on Windows when installing/uninstalling.
     if (require('electron-squirrel-startup')) {
-      // eslint-disable-line global-require
       app.quit();
     }
 
