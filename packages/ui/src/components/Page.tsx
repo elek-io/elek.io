88e5f4123794da5923fbfb495c0484a0a3bf2783
Nils Kolvenbach <info@nils-kolvenbach.de>
2022-12-01 20:09:15 +0530

    Added text

--- packages/ui/src/components/Page.tsx
+++ packages/ui/src/components/Page.tsx
@@ -18,19 +18,14 @@ export function Page(props: PageProps) {
   return (
     <>
       <main className="flex-1 overflow-y-auto">
-        {/* Primary column */}
-        <section
-          aria-labelledby="primary-heading"
-          className="flex h-full min-w-0 flex-1 flex-col lg:order-last"
-        >
-          <h1 id="primary-heading">Photos</h1>
-          {/* Your content */}
-        </section>
+        <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+          Hello from the page
+        </h1>
       </main>
 
       {/* Secondary column (hidden on smaller screens) */}
       <aside className="hidden w-96 overflow-y-auto border-l border-gray-200 bg-white lg:block">
-        {/* Your content */}
+        <span>Aside content</span>
       </aside>
     </>
   );
