ef386619fde80adcc4ce902d44b78b702024c244
Nils Kolvenbach <info@nils-kolvenbach.de>
2022-12-03 17:15:03 +0530

    Added profile dropdown with avatar to header

--- packages/ui/src/components/Header.tsx
+++ packages/ui/src/components/Header.tsx
@@ -3,12 +3,16 @@ import { HomeIcon, UserGroupIcon } from '@heroicons/react/24/outline';
 import {
   Bars3BottomLeftIcon,
   CalendarIcon,
+  ChevronDownIcon,
   MagnifyingGlassCircleIcon,
   MagnifyingGlassIcon,
   MapIcon,
   MegaphoneIcon,
   PlusIcon,
 } from '@heroicons/react/20/solid';
+import { Avatar } from './Avatar';
+import { Button } from './Button';
+import { Dropdown, itemGroupsExample } from './Dropdown';
 
 const styles = cva('', {
   variants: {
@@ -127,13 +131,12 @@ export function Header(props: HeaderProps) {
                   </Transition>
                 </Menu> */}
 
-            <button
-              type="button"
-              className="flex items-center justify-center rounded-full bg-brand-600 p-1 text-white hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2"
-            >
-              <PlusIcon className="h-6 w-6" aria-hidden="true" />
-              <span className="sr-only">Add file</span>
-            </button>
+            <Dropdown itemGroups={itemGroupsExample}>
+              <Button intent="avatar" appendIcon={ChevronDownIcon}>
+                <Avatar name="John Doe"></Avatar>
+                <span className="ml-2 hidden lg:block">John Doe</span>
+              </Button>
+            </Dropdown>
           </div>
         </div>
         <button
