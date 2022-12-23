1646eb88090914d1ccc941172268d4be8020c73b
Nils Kolvenbach <info@nils-kolvenbach.de>
2022-12-23 23:21:10 +0100

    Rewritten Page Aside, added EmptyState Component

--- apps/client-frontend/src/pages/projects/index.tsx
+++ apps/client-frontend/src/pages/projects/index.tsx
@@ -1,14 +1,27 @@
-import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline';
+import { FolderPlusIcon } from '@heroicons/react/24/outline';
 import Project from 'core/dist/esm/model/Project';
 import { PaginatedList } from 'core/dist/esm/type/service';
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { Page } from 'ui';
+import { Page, PageAside, EmptyState } from 'ui';
 
 const Projects: NextPage = () => {
   const [projects, setProjects] = useState<PaginatedList<Project>>();
+  const [aside, setAside] = useState<PageAside>();
   const [isLoading, setLoading] = useState(false);
+
+  async function onCreateProject() {
+    setAside({
+      title: 'Create Project',
+      content: createProjectContent(),
+      set: setAside,
+    });
+  }
+
+  function createProjectContent() {
+    return <h1>Create content</h1>;
+  }
 
   useEffect(() => {
     setLoading(true);
@@ -32,23 +45,26 @@ const Projects: NextPage = () => {
   if (!projects) return <p>No projects</p>;
 
   return (
-    <Page title="Projects" aside="Test of aside content">
+    <Page title="Projects" aside={aside}>
       <ul
         role="list"
-        className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3"
       >
-        <Link href="/projects/2ab08e48-435e-49b0-a3bb-667c811167c2">
-          <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
-            Test
-          </li>
-        </Link>
+        <li>
+          <EmptyState
+            icon={FolderPlusIcon}
+            title="No Projects"
+            description="Get started by creating a project"
+            onClick={onCreateProject}
+          ></EmptyState>
+        </li>
         {projects.list.map((project: Project) => (
           <Link href={`/projects/${project.id}`} key={project.id}>
             <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
               <div className="flex flex-1 flex-col p-8">
-                <h3 className="mt-6 text-sm font-medium text-gray-900">
+                <h2 className="mt-6 text-sm font-medium text-gray-900">
                   {project.name}
-                </h3>
+                </h2>
                 <dl className="mt-1 flex flex-grow flex-col justify-between">
                   <dt className="sr-only">Title</dt>
                   <dd className="text-sm text-gray-500">{project.name}</dd>
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -5,6 +5,7 @@ export * from './layouts/BaseLayout';
 export * from './components/Avatar';
 export * from './components/Button';
 export * from './components/Dropdown';
+export * from './components/EmptyState';
 export * from './components/Notification';
 export * from './components/Page';
 export * from './components/Slideover';
